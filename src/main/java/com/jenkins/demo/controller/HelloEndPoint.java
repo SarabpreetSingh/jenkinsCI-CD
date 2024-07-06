3c8d71e513a8b2ad32f48510acb2169c7c5bb7a9
Sarabpreet Singh <ssarabpreet89@gmail.com>
2024-07-06 14:55:54 +0530

    adding git ignore file

--- src/main/java/com/jenkins/demo/controller/HelloEndPoint.java
+++ src/main/java/com/jenkins/demo/controller/HelloEndPoint.java
@@ -3,6 +3,7 @@ package com.jenkins.demo.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloEndPoint {
@@ -11,5 +12,11 @@ public class HelloEndPoint {
     ResponseEntity<String> hello(@PathVariable String name){
         return ResponseEntity.ok(name);
     }
+
+    @GetMapping("/get")
+    ResponseEntity<String> helloWorld(@RequestParam String name){
+        return ResponseEntity.ok(name);
+    }
+
 
 }
